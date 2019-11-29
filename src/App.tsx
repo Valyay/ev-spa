27b96407a05bbd008e0d9d013990d12d81bf959a
Valyay <evgesha.valyaev@gmail.com>
2019-11-29 21:27:42 +0300

    Add LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route component={LoginPage} />
+        </Switch>
+    );
 };
 
 export default App;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import { withFormik, FormikProps } from 'formik';
+import * as Yup from 'yup';
+
+interface LoginValues {
+    email: string;
+    password: string;
+}
+
+interface LoginProps {
+    initialEmail?: string;
+    initialPassword?: string;
+}
+
+export const LoginForm = (props: FormikProps<LoginValues>) => {
+    const {
+        values,
+        touched,
+        dirty,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset,
+        isSubmitting
+    } = props;
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email" style={{ display: 'block' }}>
+                Email
+            </label>
+            <input
+                id="email"
+                placeholder="Enter your email"
+                type="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+            />
+            <label htmlFor="password" style={{ display: 'block' }}>
+                Password
+            </label>
+            <input
+                id="password"
+                placeholder="Enter your password"
+                type="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+            />
+            <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+            >
+                Reset
+            </button>
+            <button
+                type="submit"
+                disabled={
+                    isSubmitting ||
+                    !!(errors.email && touched.email) ||
+                    !!(errors.password && touched.password)
+                }
+            >
+                Login
+            </button>
+        </form>
+    );
+};
+
+const LoginPage = withFormik<LoginProps, LoginValues>({
+    mapPropsToValues: props => ({
+        email: props.initialEmail || '',
+        password: props.initialPassword || ''
+    }),
+
+    validationSchema: Yup.object().shape({
+        email: Yup.string()
+            .email('Email not valid')
+            .required('Email is required'),
+        password: Yup.string().required('Password is required')
+    }),
+
+    handleSubmit({ email, password }: LoginValues, { props, setSubmitting, setErrors }) {
+        console.log(email, password);
+    }
+})(LoginForm);
+
+export default LoginPage;
